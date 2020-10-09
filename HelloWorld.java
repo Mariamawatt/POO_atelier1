25f32ce3b1de37e8d652fff28f4006701af21313
Mariamawatt <71082574+Mariamawatt@users.noreply.github.com>
2020-10-09 16:08:50 +0200

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package atelier1de;
+
+/**
+ * @author Mariama
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int ma_variable = 5; // int est un type primitif
+		//byte = 1oct, short =2octs, int =32bits, long=64bits : types primitifs entiers
+		//float, double: réels
+		//boolean : 2 valeurs possibles => True ou False
+		//char : caractères
+		String maChaine = "Hello";
+		System.out.println("Bonjour tout le monde, ma valeur est:" +ma_variable);
+
+	}
+
+}
+
